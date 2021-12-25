f1d4e0486c9ac64a2215d290584cf26e97c25152
angelDariaux <angeldariaux@gmail.com>
2021-12-25 17:09:30 -0600

    terminado el punto 4, invertir el order y mostralo en pantalla

--- arraysEjercicios/arraysEjerciciosJs/ejercicio1.js
+++ arraysEjercicios/arraysEjerciciosJs/ejercicio1.js
@@ -4,7 +4,7 @@
   Hacer un programa que: 
   1) pida 6 numeros por pantalla y los meta en un array                           ok
   2) mostrar todos sus elementos en el cuerpo de la pagina y en la consola.       ok
-  3) tambien ordernarlo y mostrarlo.                                              
+  3) tambien ordernarlo y mostrarlo.                                              ok
   4) invertir su orden y mostrarlo.                                               
   5) mostrar cuantos elementos tiene el array.                                      
   6) hacer una busqueda de un valor introducido por el usuario, y que nos diga si esta en el array y su indice
@@ -20,13 +20,27 @@ for (let index = 0; numeros.length < 5 ; index++) {
   document.write(`<li>${valores}</li>`);
 };
 
-console.log(`Estos son los numeros del array:` , numeros);
+console.group(`Numeros en el orde original`);
+  console.log(`Estos son los numeros del array:` , numeros);
+console.groupEnd();
+
+
+      //Aqui ordenamos y mostramos los numeros ingresados
 
 let numerosOrdenados = [...numeros].sort((a, b) => b - a); /* con esta funcion anonima se ordean los numeros */
-console.log(`Array pero ordenado`, numerosOrdenados);
+console.group(`Numeros ordenados`);
+  console.log(`Array pero ordenado`, numerosOrdenados);
+console.groupEnd();
 
 document.write(`<h3> Listado de numeros ORDENADOS`);
-numerosOrdenados.forEach(index => document.write(`<li> ${index}</li>` ));
+numerosOrdenados.forEach(index => document.write(`<li> ${index}</li>` )); //con forEach hacemos que nos quede una lista vertical
 
+// Aqui los invertimos y los mostramos
 
+let numerosInvertidos = [...numerosOrdenados].reverse();
+console.group(`Numeros invertidos`);
+  console.log(numerosInvertidos)
+console.groupEnd();
 
+document.write( `<h3>Numeros ordenados pero INVERTIDOS</h3>` );
+numerosInvertidos.forEach(index => document.write(`<li> ${index}</li>` )); //con forEach hacemos que nos quede una lista vertical
