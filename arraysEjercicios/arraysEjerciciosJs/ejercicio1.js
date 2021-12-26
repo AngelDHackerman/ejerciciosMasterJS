4df96b0696476cc66ee832573debf81fe95cd476
angelDariaux <angeldariaux@gmail.com>
2021-12-25 22:18:57 -0600

    terminado el punto 6 del ejercicio, verificar el si el numero existe y su numero de indice en el array

--- arraysEjercicios/arraysEjerciciosJs/ejercicio1.js
+++ arraysEjercicios/arraysEjerciciosJs/ejercicio1.js
@@ -10,6 +10,8 @@
   6) hacer una busqueda de un valor introducido por el usuario, y que nos diga si esta en el array y su indice
 */
 
+      //Paso N.1 , 2
+
 let numeros = [];
 
 document.write(`<h1>Estos son los numeros ingresados:</h1>`);
@@ -23,7 +25,7 @@ console.group(`Numeros en el orde original`);
   console.log(`Estos son los numeros del array:` , numeros);
 console.groupEnd();
 
-      //Aqui ordenamos y mostramos los numeros ingresados
+      //Aqui ordenamos y mostramos los numeros ingresados Paso N.3
 
 let numerosOrdenados = [...numeros].sort((a, b) => b - a); /* con esta funcion anonima se ordean los numeros */
 console.group(`Numeros ordenados`);
@@ -33,7 +35,7 @@ console.groupEnd();
 document.write(`<h3> Listado de numeros ORDENADOS`);
 numerosOrdenados.forEach(index => document.write(`<li> ${index}</li>` )); //con forEach hacemos que nos quede una lista vertical
 
-      // Aqui los invertimos y los mostramos
+      // Aqui los invertimos y los mostramos Paso N.4
 
 let numerosInvertidos = [...numerosOrdenados].reverse();
 console.group(`Numeros invertidos`);
@@ -43,7 +45,7 @@ console.groupEnd();
 document.write( `<h3>Numeros ordenados pero INVERTIDOS</h3>` );
 numerosInvertidos.forEach(index => document.write(`<li> ${index}</li>` )); //con forEach hacemos que nos quede una lista vertical
 
-      //Aqui mostramos cuantos elementos tiene el array
+      //Aqui mostramos cuantos elementos tiene el array Paso N.5
 
 let numeroElementos = numeros.length;
 console.group(`Numero de elementos en el array:`);
@@ -52,3 +54,35 @@ console.groupEnd();
 
 document.write( `<h3>Numeros de ELEMENTOS en el array</h3>` );
 document.write( `<li>${numeroElementos}</li>` );
+
+      //Aqui hacemos la busqueda del elemento y nos dice si existe, si asi es muestra su numero de indice. Paso N.6
+
+const buscar = () => { 
+  let numberToSearch = parseInt(prompt(`Que numero debemos buscar?`));
+  const found = numeros.find(element => element == numberToSearch);
+
+  
+  if (found) {
+    let indexOriginal = numeros.indexOf(found);
+    let indexOrdered = numerosOrdenados.indexOf(found);
+    let indexInverted = numerosInvertidos.indexOf(found);
+
+    console.group(`Numero encontrado en el array (Recuerda comenzamos desde el 0)`);
+      console.log( `Este es el numero buscado y encontrado: ${found}`);
+      console.log( `Y su numero de indice en el array original es: ${indexOriginal}` );
+      console.log( `Su numero de indice ya ordenado es: ${indexOrdered}` );
+      console.log( `Su numero de indice con el array invertido es: ${indexInverted}` );
+      console.groupEnd();
+
+      document.write( `<h3>Index del ${found} en los diferentes Arrays: (Recuerda comenzamos desde el 0)</h3>` );
+      document.write( `<li>N. de index de  en el array ORIGINAL: ${indexOriginal} </li>` );
+      document.write( `<li>N. de index ORDENADO: ${indexOrdered} </li>` );
+      document.write( `<li>N. de index INVERTIDO: ${indexInverted} </li>` );
+  } else { 
+    console.log(`El numeor ${numberToSearch} no existe en el array :(`);
+    document.write( `<h3>El numero ${numberToSearch} existe en el array :( </h3>` )
+  }
+  
+  
+};
+buscar();
